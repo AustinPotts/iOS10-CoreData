30154c0ee514727c0c6cd092c295610af3e34c51
AustinPotts <pottsevolvements@gmail.com>
2019-10-15 13:48:44 -0400

    Configured TableView

--- Tasks Starter/Tasks/View Controllers/TasksTableViewController.swift
+++ Tasks Starter/Tasks/View Controllers/TasksTableViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Andrew R Madsen. All rights reserved.
 //
 
+//ios10
 import UIKit
 import CoreData
 
@@ -13,24 +14,52 @@ import CoreData
 class TasksTableViewController: UITableViewController {
     
     var taskController = TaskController()
+    
+    
+    
+    
+    lazy var fetchResultController: NSFetchedResultsController<Task> = {
+        
+        //Create the fetch request
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        //Sort Fetched Results
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "priority", ascending: true)] //priorty is name of attribute
+        
+        
+        // YOU MUST make the descriptor with the same key path as the sectionNameKeyPath be the first sort descriptor in this array
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.share.mainContext, sectionNameKeyPath: "priority", cacheName: nil)
+        
+        frc.delegate = self
+        
+        do {
+            try frc.performFetch()
+        } catch {
+            fatalError("Error performing fetch for FRC: \(error)")
+        }
+        
+        
+        return frc
+        
+    }()
+    
 
     // NOTE! This is not a good, efficient way to do this, as the fetch request
     // will be executed every time the tasks property is accessed. We will
     // learn a better way to do this later.
-    var tasks: [Task] {
-        
-        let fetchRequest: NSFetchRequest<Task> =  Task.fetchRequest()
-        
-        let moc = CoreDataStack.share.mainContext
-        
-        do {
-        let tasks = try moc.fetch(fetchRequest)
-            return tasks
-        } catch {
-            NSLog("Error fetching tasks: \(error)")
-            return []
-        }
-    }
+//    var tasks: [Task] {
+//
+//        let fetchRequest: NSFetchRequest<Task> =  Task.fetchRequest()
+//
+//        let moc = CoreDataStack.share.mainContext
+//
+//        do {
+//        let tasks = try moc.fetch(fetchRequest)
+//            return tasks
+//        } catch {
+//            NSLog("Error fetching tasks: \(error)")
+//            return []
+//        }
+//    }
     
 
     
@@ -43,17 +72,29 @@ class TasksTableViewController: UITableViewController {
     // MARK: - Table view data source
 
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchResultController.sections?.count ?? 0
+    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return tasks.count
+        return fetchResultController.sections?[section].numberOfObjects ?? 0
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        guard let sectionInfo = fetchResultController.sections?[section] else {return nil}
+        
+        return sectionInfo.name.capitalized
+        
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
 
-        cell.textLabel?.text = tasks[indexPath.row].name
+        
+        cell.textLabel?.text = fetchResultController.object(at: indexPath).name
 
         return cell
     }
@@ -70,7 +111,7 @@ class TasksTableViewController: UITableViewController {
             if let detailVC = segue.destination as? TaskDetailViewController,
                 let indexPath = tableView.indexPathForSelectedRow {
                 
-                detailVC.task = tasks[indexPath.row]
+                detailVC.task = fetchResultController.object(at: indexPath)
                 detailVC.taskController = taskController
             } else if segue.identifier == "ShowCreateTask" {
                 if let detailVC = segue.destination as? TaskDetailViewController {
@@ -85,4 +126,59 @@ class TasksTableViewController: UITableViewController {
     }
     
 
+}
+
+
+//Create Code snippet
+extension TasksTableViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else{return}
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            
+        case .delete:
+            guard let indexPath = indexPath else{return}
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        case .move:
+            guard let indexPath = indexPath,
+                let newIndexPath = newIndexPath else{return}
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+            
+        case .update:
+            guard let indexPath = indexPath else{return}
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+            
+        @unknown default:
+            return
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        let sectionSet = IndexSet(integer: sectionIndex)
+        
+        switch type {
+        case .insert:
+            tableView.insertSections(sectionSet, with: .automatic)
+            
+        case .delete:
+            tableView.deleteSections(sectionSet, with: .automatic)
+            
+        default: return
+        }
+        
+    }
+    
 }
